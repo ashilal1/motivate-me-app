6359013fbae7a8dd10c2ac5ff04141de8cec1173
ashilal1 <ashishlalshrestha10ban@gmail.com>
2025-08-06 03:46:34 +0900

    これ

---
+++ motivateme/src/main/java/com/example/motivateme/MotivatemeApplication.java
@@ -0,0 +1,13 @@
+package com.example.motivateme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotivatemeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotivatemeApplication.class, args);
+	}
+
+}
